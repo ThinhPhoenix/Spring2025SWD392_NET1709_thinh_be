24b5652d30784eef5f73dec6529aab87d78826c7
Zapz31 <hgiap46@gmail.com>
2025-01-11 22:06:44 +0700

    Initial commit for cvsts

---
+++ src/main/java/com/fpt/swd392/cvsts/CvstsApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.swd392.cvsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvstsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvstsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/swd392/cvsts/CvstsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.swd392.cvsts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvstsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d598e8e407c36f6d2ceb954e5854ecd9205500e6
ThinhPhoenix <laithinh1310@gmail.com>
2025-02-16 13:01:02 +0700

    chore: /ping -> message: pong

--- src/main/java/com/fpt/swd392/cvsts/controllers/TestController.java
+++ src/main/java/com/fpt/swd392/cvsts/controllers/TestController.java
@@ -1,16 +1,18 @@
 package com.fpt.swd392.cvsts.controllers;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class Test {
+@RequestMapping("/ping")
+public class TestController {
 
-    @GetMapping("/a")
-    public String getAllUserAccount() {
-        return "Hello";
+    @GetMapping
+    public Map<String, String> getPong() {
+        return Map.of("message", "pong");
     }
 
 }
